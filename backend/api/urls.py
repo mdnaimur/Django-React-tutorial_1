bf1d3d78fc79d6e208815eda91b202a614d6cdfb
mdnaimur <mdnaimurr@gmail.com>
2024-05-06 15:05:23 +0600

    font end added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-          path('notes/',views.NoteListCreate.as_view(),name='note-list'),     
-          path('notes/delete/<int:pk>/',views.NoteListCreate.as_view(),name='note-delete'),     
-  
-]
+    path('notes/', views.NoteListCreate.as_view(), name='note-list'),
+    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
 
+]
